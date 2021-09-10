e110ad7ae76a870fe4f88126ef55a7a635f5bc7c
vova4 <435335vova@gmail.com>
2021-09-10 20:47:22 +0300

    Создание сканера и разрешеня камеры для устройст с api > 23.

--- app/src/main/java/com/example/qrtest/MainActivity.kt
+++ app/src/main/java/com/example/qrtest/MainActivity.kt
@@ -1,11 +1,113 @@
 package com.example.qrtest
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+
+private const val CAMERA_REQUEST_CODE = 101
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
+    private val codeScannerView by lazy { findViewById<CodeScannerView>(R.id.code_scanner) }
+    private val scannerTv by lazy { findViewById<TextView>(R.id.scanner_tv) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupPermissions()
+        codeScanner()
+    }
+
+    private fun codeScanner() {
+        codeScanner = CodeScanner(this, codeScannerView)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    scannerTv.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e("main", "Camera initialization error: ${it.message}")
+                }
+            }
+
+            scannerTv.setOnClickListener {
+                codeScanner.startPreview()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    // Функция разрешения камеры для устройств api > 23
+    private fun setupPermissions() {
+        val permissions = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        )
+
+        if (permissions != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this,
+                        "Вам нужно разрешить доступ к камере чтобы использовать это приложение",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    // successful
+                }
+            }
+        }
     }
 }
